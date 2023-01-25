ec0490b81872f65c15de21a45699faf221cf6cc5
Anisha <Anisha@LAPTOP-EVNAEDUA>
2023-01-25 15:29:24 +0530

    Add project files.

---
+++ BookWeb/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BookWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!!");
+        }
+    }
+}
